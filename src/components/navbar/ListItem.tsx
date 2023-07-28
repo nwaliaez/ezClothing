579a2f3d0bd46259796bc710633f5cafeb04e732
Kuldeep Dhaka <kuldeep.dhaka@techinfinito.com>
2023-07-28 20:14:22 +0530

    navbar updated

--- src/components/navbar/ListItem.tsx
+++ src/components/navbar/ListItem.tsx
@@ -1,16 +1,18 @@
 import { FC, ReactNode } from "react";
 import { Button } from "../ui/button";
-import Link from 'next/link';
+import Link from "next/link";
 
 interface ListItemProps {
-    href: string;
-    children: ReactNode;
+  href: string;
+  children: ReactNode;
 }
 
 const ListItem: FC<ListItemProps> = ({ href, children }) => {
-    return (
-       <Button variant="ghost"><Link href={href}>{children}</Link></Button>
-    );
+  return (
+    <Button variant="ghost" asChild>
+      <Link href={href}>{children}</Link>
+    </Button>
+  );
 };
 
 export default ListItem;
